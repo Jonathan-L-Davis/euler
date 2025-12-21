51061e789780e8286f415be3472cda8c5e8d11b0
Jon Davis <jldavisjr@yahoo.com>
2025-12-21 14:30:52 -0500

    WAYYYY faster. thx fake square root!

--- src/prime_sieve.cpp
+++ src/prime_sieve.cpp
@@ -46,14 +46,18 @@ void load_primes(){
 }
 
 // I know this is not even close to optimal.
-int num_bytes(uint64_t n){
+int num_bits(uint64_t n){
     int upper_1 = 0;
     
     for(int i = 0; i < 64; i++){
         if((n>>i)&1) upper_1 = i;
     }
     
-    return upper_1/8+1;
+    return upper_1;
+}
+
+int num_bytes(uint64_t n){
+    return num_bits(n)/8+1;
 }
 
 // want to avoid rewriting/saving lower primes that we know are complete.
@@ -89,19 +93,43 @@ int main(int argc, char** argv){
     
     uint64_t saved = primes.size();
     
-    uint64_t start = saved?primes[saved-1]+1:2;
+    uint64_t start = saved?primes[saved-1]:2;
+    
+    if(start==2){
+        start=3;
+        primes={2};// makes it easier to skip evens later.
+    }else start += 2;// skip over a few.
     
     std::cout << "Have already computed " << saved << " PRIME numbers!!!\n";
     std::cout << "Starting at: " << start << std::hex << " : 0x" << start << std::dec << "\n";
     
-    for( uint64_t i = start; i < 0xFFFF'FFFF; i++){
+    assert(start&1 && "Uh oh, why are we starting on an even number?");
+    for( uint64_t i = start; i < 0xFFFF'FFFF; i+=2){
         bool is_prime = true;
-        for(auto P : primes)
+        
+        // should always be greater than the real sqrt & is a relatively accurate approx for how cheap it is.
+        // Conceptually, given i, we take the next power of 2 greater, then square root it with bit shifts.
+        // Then we cut off half the bits ( I do this with a single bit shift of half the expected bits).
+        // +1 because numbits returns one less than you'd expect (it's hightest bit pos).
+        // +1 again in case we have an odd number of bits. (otherwise the div by 2 would put us under the square root. Basically a no op for even number of bits)
+        // instead of shifting over all the way and half way back I just do a single left shift of half length.
+        // can definitely do better for similar complexity, but this is okay for now.
+        uint64_t fake_sqrt = 1<<( (num_bits(i)+2)/2);
+        
+        for(auto P : primes){
+            
+            if(P>fake_sqrt) break;
+            
             if(i%P==0)
                 is_prime = false;
+            
+            if(!is_prime)break;// early out because I don't want to waste time.
+        }
+        
         if( is_prime ) primes.push_back(i);
         
-        if( saved+1024 <= primes.size()){
+        constexpr uint64_t group_size = 4096*4;
+        if( saved+group_size <= primes.size()){
             save_primes();
             saved = primes.size();
             
